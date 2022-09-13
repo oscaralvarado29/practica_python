62a4db4c9073796799d2520defdff7044a887eeb
oscaralvarado29 <oscar.alvarado.mt@correo.usa.edu.co>
2022-09-12 21:57:51 -0500

    ejercicios python

---
+++ python.py
@@ -0,0 +1,15 @@
+# ejemplo de variables
+"""
+mensaje = "Hola Mundo estoy aprendiendo python"
+print(mensaje)
+
+nombre = "Juan"
+apellido = "Perez"
+ciudad = "Buenos Aires"
+celular = 300287649
+# print(nombre, " ", apellido, ciudad, numero)
+print(f"hola {nombre} {apellido} de la ciudad {ciudad} con celular {celular}")
+print("hola %s de %s" % (nombre, ciudad))
+"""
+nombre = input("Ingrese su nombre: ")
+print("Hola", nombre)
